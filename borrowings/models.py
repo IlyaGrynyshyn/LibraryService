b5847479be4d4e444a82de30c4b447f9269e3511
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-05 23:39:13 +0200

    Implement Borrowing model in borrowings app

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.borrow_date} - {self.user}"
