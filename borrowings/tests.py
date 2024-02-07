4fc53e469136cc89c20f85b775203dead4aef0a4
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-07 20:12:39 +0200

    Implement test for borrowing view

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework import status
+
 from books.models import Book
-from .models import Borrowing
+from borrowings.models import Borrowing
 
 
 class BorrowingModelTest(TestCase):
@@ -21,25 +27,113 @@ class BorrowingModelTest(TestCase):
         borrowing = Borrowing.objects.create(
             borrow_date="2022-01-01",
             expected_return_date="2022-02-01",
-            actual_return_date="2022-01-15",
-            book_id=self.book,
+            book=self.book,
             user=self.user,
         )
-
-        self.assertEqual(str(borrowing), "2022-01-01 - test@user.com")
+        self.assertEqual(str(borrowing), f"{timezone.now().date()} - test@user.com")
 
     def test_borrowing_model_fields(self):
         borrowing = Borrowing.objects.create(
             borrow_date="2022-01-01",
             expected_return_date="2022-02-01",
-            actual_return_date="2022-01-15",
-            book_id=self.book,
+            book=self.book,
             user=self.user,
         )
 
         saved_borrowing = Borrowing.objects.get(pk=borrowing.id)
-        self.assertEqual(str(saved_borrowing.borrow_date), "2022-01-01")
+        self.assertEqual(str(saved_borrowing.borrow_date), str(timezone.now().date()))
         self.assertEqual(str(saved_borrowing.expected_return_date), "2022-02-01")
-        self.assertEqual(str(saved_borrowing.actual_return_date), "2022-01-15")
-        self.assertEqual(saved_borrowing.book_id, self.book)
+        self.assertEqual(saved_borrowing.book, self.book)
         self.assertEqual(saved_borrowing.user, self.user)
+
+
+class BorrowingViewTests(TestCase):
+    BORROWING_URL = reverse("borrowings:borrowing-list")
+
+    def setUp(self):
+        self.client = APIClient()
+        user = get_user_model()
+        self.admin_user = user.objects.create_user(
+            email="admin@example.com", password="adminpassword", is_staff=True
+        )
+        self.user = user.objects.create_user(
+            email="user@example.com", password="userpass"
+        )
+        self.book = Book.objects.create(
+            title="Sample Book",
+            author="John Doe",
+            cover=Book.CoverType.HARD,
+            inventory=12,
+            daily_fee="19.99",
+        )
+
+        self.admin_token = self.generate_jwt_token(self.admin_user)
+        self.user_token = self.generate_jwt_token(self.user)
+
+    def generate_jwt_token(self, user):
+        refresh = RefreshToken.for_user(user)
+        access_token = AccessToken.for_user(user)
+        return {
+            "refresh": str(refresh),
+            "access": str(access_token),
+        }
+
+    def test_create_borrowing(self):
+        self.client.credentials(HTTP_AUTHORIZE=f'Bearer {self.user_token["access"]}')
+
+        initial_inventory = self.book.inventory
+
+        payload = {
+            "borrow_date": "2024-02-06",
+            "expected_return_date": "2024-02-10",
+            "book": self.book.id,
+        }
+
+        response = self.client.post(self.BORROWING_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        borrowing = Borrowing.objects.get(id=response.data["id"])
+        updated_book = Book.objects.get(id=self.book.id)
+
+        self.assertEqual(borrowing.user, self.user)
+        self.assertEqual(updated_book.inventory, initial_inventory - 1)
+
+    def test_create_borrowing_with_zero_inventory(self):
+        self.client.credentials(HTTP_AUTHORIZE=f'Bearer {self.user_token["access"]}')
+
+        book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="Hard",
+            inventory=0,
+            daily_fee=10.0,
+        )
+
+        payload = {
+            "borrow_date": "2024-02-06",
+            "expected_return_date": "2024-02-10",
+            "book": book.id,
+        }
+
+        response = self.client.post(self.BORROWING_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn(
+            "Unfortunately, this book is out of stock.",
+            response.data["non_field_errors"],
+        )
+
+    def test_retrieve_borrowing(self):
+        self.client.credentials(HTTP_AUTHORIZE=f'Bearer {self.user_token["access"]}')
+        borrowing = Borrowing.objects.create(
+            borrow_date="2022-01-01",
+            expected_return_date="2022-02-01",
+            user=self.user,
+            book=self.book,
+        )
+
+        response = self.client.get(
+            reverse("borrowings:borrowing-detail", kwargs={"pk": borrowing.pk})
+        )
+        self.assertEqual(response.status_code, 200)
