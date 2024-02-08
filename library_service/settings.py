acd460e1912383471378cc47fc4de010d8b56fd4
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-08 21:17:07 +0200

    Implement pagination

--- library_service/settings.py
+++ library_service/settings.py
@@ -131,6 +131,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 24,
 }
 
 SPECTACULAR_SETTINGS = {
